40e7587f4a34224e53b29f7e0d8421523eb3ab1c
openfugjoobot <openfugjoobot@gmail.com>
2026-02-27 11:12:18 +0100

    Fix white screen: disable empty onboarding component

--- mobile/src/navigation/RootNavigator.tsx
+++ mobile/src/navigation/RootNavigator.tsx
@@ -1,21 +1,19 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { useOnboardingCompleted } from '../stores/app';
 import { TabNavigator } from './TabNavigator';
 import type { RootStackParamList } from './types';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export const RootNavigator: React.FC = () => {
-  const onboardingCompleted = useOnboardingCompleted();
+  // FIXME: Onboarding temporarily disabled - white screen bug
+  // const onboardingCompleted = useOnboardingCompleted();
 
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        {!onboardingCompleted ? (
-          <Stack.Screen name="Onboarding" component={() => null} />
-        ) : null}
+        {/* Onboarding disabled until implemented */}
         <Stack.Screen name="MainTabs" component={TabNavigator} />
       </Stack.Navigator>
     </NavigationContainer>
