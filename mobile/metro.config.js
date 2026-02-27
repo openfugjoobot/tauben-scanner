8141c9773c45902007236e91c817a5e31d700870
openfugjoobot <openfugjoobot@gmail.com>
2026-02-27 09:47:11 +0100

    Fix axios crypto issue: force browser build in Metro

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -4,4 +4,17 @@ const { getDefaultConfig } = require('expo/metro-config');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
+// Fix axios crypto module issue in React Native
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  // Force axios to use browser build instead of node build
+  if (moduleName === 'axios') {
+    return {
+      filePath: require.resolve('axios/dist/browser/axios.cjs'),
+      type: 'sourceFile',
+    };
+  }
+  // Fall back to default resolution
+  return context.resolveRequest(context, moduleName, platform);
+};
+
 module.exports = config;
