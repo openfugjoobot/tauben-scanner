fe07a055d9cd4df1512979f98f57c99773f0c0ea
openfugjoobot <openfugjoobot@gmail.com>
2026-03-02 12:49:33 +0100

    Delete mobile/__mocks__ directory

--- mobile/__mocks__/react-native-maps.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const MapView = jest.fn(({ children }: { children: React.ReactNode }) => children);
-export const Marker = jest.fn(({ children }: { children: React.ReactNode }) => children);
-export const PROVIDER_GOOGLE = 'google';
-
-export default MapView;
